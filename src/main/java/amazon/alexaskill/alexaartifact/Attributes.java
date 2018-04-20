5303b7601561efcbda6029697b3be1ee992fd661
Supraja Kumaravel <suprajk@amazon.com>
2018-04-20 11:48:04 +0530

    Passing Map and combination in Session
    Passing Map and combination in Session

--- src/main/java/amazon/alexaskill/alexaartifact/Attributes.java
+++ src/main/java/amazon/alexaskill/alexaartifact/Attributes.java
@@ -4,6 +4,8 @@ public class Attributes {
     public static final String STATE_KEY = "state";
     public static final String GAME_COMBINATION_KEY = "gamecombination";
     public static final String GAME_SCORE_KEY = "gamescore";
+    public static final String GAME_LEVEL_KEY = "gamelevel";
+    public static final String GAME_MAPPED_WORDS = "mappedwords";
     //public static final String QUIZ_PROPERTY_KEY = "quizproperty";
     //public static final String COUNTER_KEY = "counter";
     public static final String RESPONSE_KEY = "response";
